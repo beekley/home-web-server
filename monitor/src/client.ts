bf0f595fc830553211374e2133a2f9e72a8f0e4b
beekley <brettbeekley@gmail.com>
2026-01-11 08:53:42 -0800

    Post metrics to monitoring service

--- monitor/src/client.ts
+++ monitor/src/client.ts
@@ -2,7 +2,7 @@ import os from "os";
 
 const MON_HOST =
   process.env.MON_HOST || "monitor.home-web-server.svc.cluster.local";
-const MON_INTERVAL = 1000; // 1 s.
+const MON_INTERVAL = 10 * 1000; // 10 s.
 
 interface MetricPayload {
   host: string; // e.g., "rpi-1"
@@ -27,7 +27,7 @@ export class Monitor {
   }
 
   public start(): void {
-    setInterval(() => this.flushBuffer(), MON_INTERVAL);
+    setInterval(async () => await this.flushBuffer(), MON_INTERVAL);
   }
 
   public recordMetric(point: Point): void {
@@ -40,25 +40,33 @@ export class Monitor {
   }
 
   private async flushBuffer(): Promise<void> {
-    const url = `https://${MON_HOST}/metric`;
+    const url = `http://${MON_HOST}/metric`;
 
-    console.log(`Sending metrics to ${url}:`, this.buffer);
-    //   try {
-    //     const response = await fetch(url, {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify(payload),
-    //     });
+    const currentBuffer: MetricPayload[] = [];
+    while (this.buffer.length > 0) {
+      currentBuffer.push(this.buffer.pop()!);
+    }
 
-    //     if (!response.ok) {
-    //       console.error(
-    //         `Failed to record metric. Server responded with status: ${response.status}`
-    //       );
-    //     }
-    //   } catch (error) {
-    //     console.error("Error sending metric to monitor:", error);
-    //   }
+    console.log("POSTING", url, currentBuffer);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(currentBuffer),
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to record metric. Server responded with status: ${response.status}`
+        );
+      }
+    } catch (error) {
+      console.error("Error sending metric to monitor:", error);
+      // Replace the unsuccessfully sent payloads.
+      this.buffer.push(...currentBuffer);
+    }
   }
 }
--- monitor/src/index.ts
+++ monitor/src/index.ts
@@ -8,28 +8,55 @@ const PORT = 3000;
 // Create the server
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-    monitor.recordMetric({
-      metric: "request_latency",
-      value: 10,
-      unit: "ms",
-    });
+    const start = Date.now();
 
     try {
       // We need a full URL to parse query parameters
       const url = new URL(req.url || "/", `http://${req.headers.host}`);
 
-      // Send the response
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(
-        `hello world from monitoring at ${
-          process.env.NODE_NAME || os.hostname()
-        }`
-      );
+      // Home
+      if (url.pathname === "/") {
+        // Send the response
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(
+          `hello world from monitoring at ${
+            process.env.NODE_NAME || os.hostname()
+          }`
+        );
+      }
+
+      // Post metrics
+      else if (url.pathname === "/metric" && req.method === "POST") {
+        const body: Buffer[] = [];
+
+        req.on("data", (chunk) => {
+          body.push(chunk);
+        });
+
+        req.on("end", () => {
+          const data = JSON.parse(Buffer.concat(body).toString());
+          console.log("Received metrics:", data);
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end(`Metrics received: ${data}`);
+        });
+      }
+
+      // 404
+      else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+      }
     } catch (error) {
       console.error("Error handling request:", error);
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Internal Server Error");
     }
+
+    monitor.recordMetric({
+      metric: "request_latency",
+      value: Date.now() - start,
+      unit: "ms",
+    });
   }
 );
 
